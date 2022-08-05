277572aee0c3d5eb649c243949ebd8aed8a31a89
Rustam <103527813+Rustam-Gazizulin@users.noreply.github.com>
2022-08-05 23:58:24 +0500

    create bd

--- main.py
+++ main.py
@@ -16,15 +16,37 @@ class UserModel(db.Model):
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
 
+    def __repr__(self):
+        return f'{self.pk}, {self.first_name}'
+
+
+class PhoneModel(db.Model):
+    __tablename__ = 'phone'
+
+    pk = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f'{self.pk}, {self.number}'
+
 
 db.drop_all()
 db.create_all()
 
 user = UserModel(pk=1, first_name='Ivan', last_name='Petrov')
 user1 = UserModel(pk=2, first_name='Roman', last_name='Romanov')
+# super_user = [user, user1]
 
-db.session.add(user1)
+phone = PhoneModel(pk=1, number='1234567890')
+
+db.session.add_all([user, user1, phone])
 db.session.commit()
+
+
+user_from_db = db.session.query(UserModel).get(1)
+phone_from_db = db.session.query(PhoneModel).get(1)
+print(user_from_db)
+print(type(phone_from_db))
 
 # @app.route('/')
 # def index():
